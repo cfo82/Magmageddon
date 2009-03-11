3ab6d8be082cbb92e3bc7eba4d8a179f1c6ac9e1
kaeserd <kaeserd@fc16ba98-2866-4055-be8f-df951a5456ab>
2009-03-11 01:37:44 +0000

    git-svn-id: https://svn.vis.ethz.ch/svn/kob_gamelab09/trunk@59 fc16ba98-2866-4055-be8f-df951a5456ab

---
+++ src/ProjectMagma/ProjectMagma/Framework/Attributes/IntAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace ProjectMagma.Framework
+{
+    class IntAttribute : Attribute
+    {
+        public IntAttribute(string name, AttributeTemplate template)
+        :   base(name, template)
+        {
+        }
+            
+        public override void Initialize(ContentManager content, string value)
+        {
+            v = int.Parse(value);
+        }
+
+        public int Value
+        {
+            get
+            {
+                return v;
+            }
+
+            set
+            {
+                v = value;
+            }
+        }
+
+        private int v;
+    }
+}

fd9032d070f05247821b40f7e5283b5ca3aeee82
kaeserd <kaeserd@fc16ba98-2866-4055-be8f-df951a5456ab>
2009-03-12 17:25:59 +0000

    git-svn-id: https://svn.vis.ethz.ch/svn/kob_gamelab09/trunk@95 fc16ba98-2866-4055-be8f-df951a5456ab

---
+++ src/ProjectMagma/ProjectMagma/Framework/Attributes/FloatAttribute.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace ProjectMagma.Framework
+{
+    public class FloatAttribute : Attribute
+    {
+        public FloatAttribute(string name)
+        :   base(name)
+        {
+        }
+            
+        public override void Initialize(string value)
+        {
+            v = float.Parse(value);
+        }
+
+        public float Value
+        {
+            get
+            {
+                return v;
+            }
+
+            set
+            {
+                if (v != value)
+                {
+                    float oldValue = v;
+                    v = value;
+                    OnValueChanged(oldValue, v);
+                }
+            }
+        }
+
+        private void OnValueChanged(float oldValue, float newValue)
+        {
+            if (ValueChanged != null)
+            {
+                ValueChanged(this, oldValue, newValue);
+            }
+        }
+
+        public event FloatChangeHandler ValueChanged;
+        private float v;
+    }
+}
---
+++ src/ProjectMagma/ProjectMagma/Framework/Event/FloatChangeHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace ProjectMagma.Framework
+{
+    public delegate void FloatChangeHandler(FloatAttribute sender, float oldValue, float newValue);
+}

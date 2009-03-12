4f94e7b1090e171d1c49dc853a9743de1b1d8ca3
kaeserd <kaeserd@fc16ba98-2866-4055-be8f-df951a5456ab>
2009-03-12 17:57:14 +0000

    git-svn-id: https://svn.vis.ethz.ch/svn/kob_gamelab09/trunk@97 fc16ba98-2866-4055-be8f-df951a5456ab

---
+++ src/ProjectMagma/ProjectMagma/Framework/Properties/IceSpikeControllerProperty.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace ProjectMagma.Framework
+{
+    public class IceSpikeControllerProperty : Property
+    {
+        public IceSpikeControllerProperty()
+        {
+        }
+
+        public void OnAttached(Entity entity)
+        {
+            entity.Update += new UpdateHandler(OnUpdate);
+        }
+
+        public void OnDetached(Entity entity)
+        {
+            entity.Update -= new UpdateHandler(OnUpdate);
+        }
+
+        private void OnUpdate(Entity entity, GameTime gameTime)
+        {
+
+            // fetch required values
+            Vector3 pos = entity.GetVector3("position");
+            Vector3 v = entity.GetVector3("velocity");
+            float dt = ((float)gameTime.ElapsedGameTime.Milliseconds)/1000.0f;
+
+            // compute acceleration
+            Vector3 a = new Vector3(straightAcceleration, 0f, 0f);
+
+            // integrate
+            v += dt*a;
+            pos += dt*v;
+
+            // store computed values;
+            entity.SetVector3("position", pos);
+            entity.SetVector3("velocity", v);
+        }
+
+        private Entity target;
+        private float straightAcceleration = 100f;
+        private float maxSpeed = 150f;
+    }
+}
---
+++ src/ProjectMagma/ProjectMagmaContentPipeline/ModelProcessors/IceSpikeProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+using Microsoft.Xna.Framework.Content.Pipeline.Processors;
+
+namespace ProjectMagmaContentPipeline.ModelProcessors
+{
+    [ContentProcessor(DisplayName = "Magma - Ice Spike Processor")]
+    class IceSpikeProcessor : MoveProcessor
+    {
+        protected override float CalculateHeightDiff(ref BoundingBox bb)
+        {
+            return 0.0f;
+        }
+    }
+}

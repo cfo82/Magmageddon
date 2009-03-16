88853a2eaac38330f076a24382d1fa5865264c93
coberhol <coberhol@fc16ba98-2866-4055-be8f-df951a5456ab>
2009-03-16 01:37:13 +0000

    git-svn-id: https://svn.vis.ethz.ch/svn/kob_gamelab09/trunk@173 fc16ba98-2866-4055-be8f-df951a5456ab

--- src/ProjectMagma/ProjectMagma/BloomSettings.cs
+++ src/ProjectMagma/ProjectMagma/BloomSettings.cs
@@ -77,7 +77,7 @@ namespace ProjectMagma
             new BloomSettings("Saturated",   0.25f,  4,   2,     1,    2,       0),
             new BloomSettings("Blurry",      0,      2,   1,     0.1f, 1,       1),
             new BloomSettings("Subtle",      0.5f,   2,   1,     1,    1,       1),
-            new BloomSettings("Custom",      0.35f,  3,   1.125f,1,    1,       1)
+            new BloomSettings("Custom",      0.4f,   3,   1.20f,1,    1,       1)
         };
     }
 }

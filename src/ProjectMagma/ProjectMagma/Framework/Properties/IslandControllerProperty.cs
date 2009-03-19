4cdfe27fc4969c060f73b9694315850dba4fedf9
jabernet <jabernet@fc16ba98-2866-4055-be8f-df951a5456ab>
2009-03-19 14:28:29 +0000

    put island constants into xml
    git-svn-id: https://svn.vis.ethz.ch/svn/kob_gamelab09/trunk@220 fc16ba98-2866-4055-be8f-df951a5456ab

--- src/ProjectMagma/ProjectMagma/Framework/Properties/IslandControllerProperty.cs
+++ src/ProjectMagma/ProjectMagma/Framework/Properties/IslandControllerProperty.cs
@@ -17,8 +17,10 @@ namespace ProjectMagma.Framework
 
         public void OnAttached(Entity entity)
         {
-            entity.Update += OnUpdate;
             entity.AddIntAttribute("collisionCount", 0);
+            this.constants = Game.Instance.EntityManager["island_constants"];
+
+            entity.Update += OnUpdate;
         }
 
         public void OnDetached(Entity entity)
@@ -27,24 +29,24 @@ namespace ProjectMagma.Framework
             // TODO: remove attribute!
         }
 
-        private void OnUpdate(Entity entity, GameTime gameTime)
+        private void OnUpdate(Entity island, GameTime gameTime)
         {
             float dt = ((float)gameTime.ElapsedGameTime.Milliseconds)/1000.0f;
 
             // control this island
             // read out attributes
-            Vector3 v = entity.GetVector3("velocity");
+            Vector3 v = island.GetVector3("velocity");
 
             // first force contribution: random               
             Vector3 a = new Vector3(
                 (float)rand.NextDouble() - 0.5f,
                 0.0f,
                 (float)rand.NextDouble() - 0.5f
-            ) * islandRandomStrength;
+            ) * constants.GetFloat("random_strength");
 
 
             // second force contribution: collision with pillars
-            Vector3 islandPosition = entity.GetVector3("position");
+            Vector3 islandPosition = island.GetVector3("position");
             bool collided = false;
 
             foreach (Entity pillar in Game.Instance.PillarManager)
@@ -65,9 +67,9 @@ namespace ProjectMagma.Framework
 
                 BoundingBox islandBox = (BoundingBox)Game.Instance.Content.Load<Model>("Models/island_primitive").Tag;
                 float islandScale = islandBox.Max.X;
-                if (entity.HasVector3("scale"))
+                if (island.HasVector3("scale"))
                 {
-                    Vector3 scale = entity.GetVector3("scale");
+                    Vector3 scale = island.GetVector3("scale");
                     Debug.Assert(scale.X == scale.Z);
                     islandScale *= scale.X;
                 }
@@ -77,17 +79,17 @@ namespace ProjectMagma.Framework
                 {
                     // no collision with this pillar
                     pillarContribution = dist;
-                    pillarContribution *= pillarContribution.Length() * pillarAttraction;
+                    pillarContribution *= pillarContribution.Length() * constants.GetFloat("pillar_attraction");
                 }
                 else
                 {
                     // island collided with this pillar
-                    pillarContribution = -dist * pillarRepulsion;// *(pillarIslandCollisionRadius - dist.Length()) * 10.0f;
-                    if (entity.GetInt("collisionCount") == 0)
+                    pillarContribution = -dist * constants.GetFloat("pillar_repulsion");// *(pillarIslandCollisionRadius - dist.Length()) * 10.0f;
+                    if (island.GetInt("collisionCount") == 0)
                     {
                         // perform elastic collision if its the first time
 
-                        v = -v * (1.0f - pillarElasticity);
+                        v = -v * (1.0f - constants.GetFloat("pillar_elasticity"));
                         //Console.WriteLine("switching dir " + (e.Attributes["collisionCount"] as IntAttribute).Value);// + " " + rand.NextDouble());
                     }
                     else
@@ -95,7 +97,7 @@ namespace ProjectMagma.Framework
                         // in this case, the island is stuck. try gradually increasing
                         // the opposing force until the island manages to escape.
 
-                        pillarContribution *= entity.GetInt("collisionCount");
+                        pillarContribution *= island.GetInt("collisionCount");
                         //Console.WriteLine("contrib " + pillarContribution);
                     }
                     collided = true;
@@ -105,35 +107,30 @@ namespace ProjectMagma.Framework
 
             if (!collided)
             {
-                entity.SetInt("collisionCount", 0);
+                island.SetInt("collisionCount", 0);
             }
             else
             {
-                entity.SetInt("collisionCount", entity.GetInt("collisionCount") + 1);
+                island.SetInt("collisionCount", island.GetInt("collisionCount") + 1);
             }
 
             // compute final acceleration
-            entity.SetVector3("acceleration", a);
+            island.SetVector3("acceleration", a);
 
             // compute final velocity
-            v = (v + dt * entity.GetVector3("acceleration")) * (1.0f - islandDamping);
+            v = (v + dt * island.GetVector3("acceleration")) * (1.0f - constants.GetFloat("damping"));
             float velocityLength = v.Length();
-            if (velocityLength > islandMaxVelocity)
+            if (velocityLength > constants.GetFloat("max_velocity"))
             {
-                v *= islandMaxVelocity / velocityLength;
+                v *= constants.GetFloat("max_velocity") / velocityLength;
             }
-            entity.SetVector3("velocity", v);
+            island.SetVector3("velocity", v);
 
             // compute final position
-            entity.SetVector3("position", entity.GetVector3("position") + dt * entity.GetVector3("velocity"));
+            island.SetVector3("position", island.GetVector3("position") + dt * island.GetVector3("velocity"));
         }
 
+        private Entity constants;
         private Random rand;
-        private static float islandRandomStrength = 1000.0f;
-        private static float islandMaxVelocity = 200;
-        private static float pillarElasticity = 0.1f;
-        private static float pillarAttraction = 0.0005f;
-        private static float pillarRepulsion = 0.03f;
-        private static float islandDamping = 0.001f;
     }
 }

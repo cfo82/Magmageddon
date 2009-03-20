ee3aef1ee3ec143ffec1279d014e86bfa00ccdd0
coberhol <coberhol@fc16ba98-2866-4055-be8f-df951a5456ab>
2009-03-20 02:34:27 +0000

    fixed some problems... newlines in string representations of attributes are not allowed.
    git-svn-id: https://svn.vis.ethz.ch/svn/kob_gamelab09/trunk@228 fc16ba98-2866-4055-be8f-df951a5456ab

--- src/ProjectMagma/ProjectMagma/Framework/Attributes/MatrixAttribute.cs
+++ src/ProjectMagma/ProjectMagma/Framework/Attributes/MatrixAttribute.cs
@@ -23,7 +23,21 @@ namespace ProjectMagma.Framework
             }
             else
             {
-                Debug.Assert(false, "String initialization of matrices is currently not supported."); // TODO
+                string[] splitArray = value.Split(' ');
+                float[] values = new float[] { 1, 0, 0, 0,  0, 1, 0, 0,  0, 0, 1, 0,  0, 0, 0, 1 };
+                for (int i = 0; i < 16; ++i)
+                {
+                    float temp = 0.0f;
+                    if (splitArray.Length > i && float.TryParse(splitArray[i], out temp))
+                    {
+                        values[i] = temp;
+                    }
+                }
+
+                v.M11 = values[ 0]; v.M12 = values[ 1]; v.M13 = values[ 2]; v.M14 = values[ 3];
+                v.M21 = values[ 4]; v.M22 = values[ 5]; v.M23 = values[ 6]; v.M24 = values[ 7];
+                v.M31 = values[ 8]; v.M32 = values[ 9]; v.M33 = values[10]; v.M34 = values[11];
+                v.M41 = values[12]; v.M42 = values[13]; v.M43 = values[14]; v.M44 = values[15]; 
             }
         }
 
@@ -50,10 +64,10 @@ namespace ProjectMagma.Framework
             get
             {
                 return String.Format(
-                    "{0} {1} {2} {3}\n" +
-                    "{0} {1} {2} {3}\n" +
-                    "{0} {1} {2} {3}\n" +
-                    "{0} {1} {2} {3}\n",
+                    "{0} {1} {2} {3}" +
+                    "{4} {5} {6} {7}" +
+                    "{8} {9} {10} {11}" +
+                    "{12} {13} {14} {15}",
                     v.M11, v.M12, v.M13, v.M14,
                     v.M21, v.M22, v.M23, v.M24,
                     v.M31, v.M32, v.M33, v.M34,

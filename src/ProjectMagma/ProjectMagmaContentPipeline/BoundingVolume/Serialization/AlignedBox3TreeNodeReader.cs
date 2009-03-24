f0b4a13de572c26605e356bdd7fb25136b114427
jabernet <jabernet@fc16ba98-2866-4055-be8f-df951a5456ab>
2009-03-24 16:23:44 +0000

    git-svn-id: https://svn.vis.ethz.ch/svn/kob_gamelab09/trunk@291 fc16ba98-2866-4055-be8f-df951a5456ab

---
+++ src/ProjectMagma/ProjectMagmaContentPipeline/BoundingVolume/Serialization/AlignedBox3TreeNodeReader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+using Microsoft.Xna.Framework.Content.Pipeline.Processors;
+using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler;
+
+namespace ProjectMagma.Shared.BoundingVolume.Serialization
+{
+    class AlignedBox3TreeNodeReader : ContentTypeReader<AlignedBox3TreeNode>
+    {
+        protected override AlignedBox3TreeNode Read(ContentReader input, AlignedBox3TreeNode existingInstance)
+        {
+            if (existingInstance == null)
+            {
+                existingInstance = new AlignedBox3TreeNode();
+            }
+
+            existingInstance.NumTriangles = input.ReadInt32();
+            existingInstance.BaseIndex = input.ReadInt32();
+            input.ReadSharedResource<UInt16[]>(delegate(UInt16[] value) { existingInstance.Indices = value; });
+            existingInstance.BoundingBox = input.ReadObject<BoundingBox>();
+            existingInstance.Left = input.ReadObject<AlignedBox3TreeNode>();
+            existingInstance.Right = input.ReadObject<AlignedBox3TreeNode>();
+
+            return existingInstance;
+        }
+    }
+}
---
+++ src/ProjectMagma/ProjectMagmaContentPipeline/BoundingVolume/Serialization/AlignedBox3TreeNodeWriter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+using Microsoft.Xna.Framework.Content.Pipeline.Processors;
+using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler;
+
+namespace ProjectMagma.Shared.BoundingVolume.Serialization
+{
+    [ContentTypeWriter]
+    class AlignedBox3TreeNodeWriter : ContentTypeWriter<AlignedBox3TreeNode>
+    {
+        protected override void Write(ContentWriter output, AlignedBox3TreeNode value)
+        {
+            output.Write(value.NumTriangles);
+            output.Write(value.BaseIndex);
+            output.WriteSharedResource<UInt16[]>(value.Indices);
+            output.WriteObject<BoundingBox>(value.BoundingBox);
+            output.WriteObject<AlignedBox3TreeNode>(value.Left);
+            output.WriteObject<AlignedBox3TreeNode>(value.Right);
+        }
+
+        public override string GetRuntimeReader(TargetPlatform targetPlatform)
+        {
+            return typeof(AlignedBox3TreeNodeReader).AssemblyQualifiedName;
+        }
+    }
+}

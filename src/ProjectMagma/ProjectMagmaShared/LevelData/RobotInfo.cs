5cd01c9416668a89099b32d3f87d9641357ddb71
jabernet <jabernet@fc16ba98-2866-4055-be8f-df951a5456ab>
2009-04-05 18:00:37 +0000

    git-svn-id: https://svn.vis.ethz.ch/svn/kob_gamelab09/trunk@419 fc16ba98-2866-4055-be8f-df951a5456ab

---
+++ src/ProjectMagma/ProjectMagmaShared/LevelData/RobotInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMagma.Shared.LevelData
+{
+    public class RobotInfo
+    {
+        private String name;
+        private String description;
+        private String fileName;
+
+        public RobotInfo(String name, String description, String fileName)
+        {
+            this.name = name;
+            this.description = description;
+            this.fileName = fileName;
+        }
+
+        public String Name
+        {
+            get { return name; }
+        }
+
+        public String Description
+        {
+            get { return description; }
+        }
+
+        public String FileName
+        {
+            get { return fileName; }
+        }
+
+    }
+}

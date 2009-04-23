7407ebf88dc7b883c44a29309b04e474494bc458
kaeserd <kaeserd@fc16ba98-2866-4055-be8f-df951a5456ab>
2009-04-23 14:42:25 +0000

    git-svn-id: https://svn.vis.ethz.ch/svn/kob_gamelab09/trunk@635 fc16ba98-2866-4055-be8f-df951a5456ab

---
+++ src/ProjectMagma/ProjectMagma/Renderer/RenderPasses/Copy of GlowPass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ProjectMagma.Renderer
+{
+    public class TestPass : RenderPass
+    {
+        public TestPass(Renderer Renderer)
+            : base(Renderer)
+        {
+        }
+
+        public override void Render(GameTime gameTime)
+        {
+            DrawFullscreenQuad(Renderer.RenderChannels, Renderer.Device.Viewport.Width, Renderer.Device.Viewport.Height / 2, null);
+            DrawFullscreenQuad(Renderer.ResolveTarget, Renderer.Device.Viewport.Width/2, Renderer.Device.Viewport.Height, null);
+        }        
+
+    }
+}

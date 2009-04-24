6e92387a8d39e516cd456ea1229e4aeef0145a40
kaeserd <kaeserd@fc16ba98-2866-4055-be8f-df951a5456ab>
2009-04-24 21:06:17 +0000

    git-svn-id: https://svn.vis.ethz.ch/svn/kob_gamelab09/trunk@648 fc16ba98-2866-4055-be8f-df951a5456ab

---
+++ src/ProjectMagma/ProjectMagma/Renderer/Renderables/EnvironmentRenderable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ProjectMagma.Shared.Math.Integration;
+
+namespace ProjectMagma.Renderer
+{
+    public class EnvironmentRenderable : TexturedRenderable
+    {
+        public EnvironmentRenderable(Vector3 scale, Quaternion rotation, Vector3 position, Model model)
+            : base(scale, rotation, position, model)
+        {
+            randomOffset = new DoublyIntegratedVector2
+            (
+                Vector2.Zero, Vector2.Zero, 0.0f, 0.0f, -1.0f, 1.0f
+            );
+            RenderChannel = RenderChannelType.One;
+        }
+
+        //protected override void ApplyEffectsToModel()
+        //{
+        //    Effect effect = Game.Instance.Content.Load<Effect>("Effects/Environment/Island");
+        //    SetDefaultMaterialParameters();
+        //    SetModelEffect(Model, effect);
+        //}
+       
+        protected override void ApplyCustomEffectParameters(Effect effect, Renderer renderer, GameTime gameTime)
+        {
+            base.ApplyCustomEffectParameters(effect, renderer, gameTime);
+
+            randomOffset.RandomlyIntegrate(gameTime, 0.0008f, 0.0f);
+            effect.Parameters["RandomOffset"].SetValue(randomOffset.Value);
+        }
+
+        //public float WindStrength { get; set; }
+        
+        private DoublyIntegratedVector2 randomOffset;
+    }
+}

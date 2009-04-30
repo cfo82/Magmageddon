d9f2b8289bc8187ddc965dc7bfff5ff257354205
jabernet <jabernet@fc16ba98-2866-4055-be8f-df951a5456ab>
2009-04-30 14:55:25 +0000

    fixed explosion positioning
    git-svn-id: https://svn.vis.ethz.ch/svn/kob_gamelab09/trunk@755 fc16ba98-2866-4055-be8f-df951a5456ab

---
+++ src/ProjectMagma/ProjectMagmaContentPipeline/ModelProcessors/ExplosionProcessor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+using Microsoft.Xna.Framework.Content.Pipeline.Processors;
+
+namespace ProjectMagma.ContentPipeline.ModelProcessors
+{
+    [ContentProcessor(DisplayName = "Magma - Explosion Processor")]
+    public class ExplosionProcessor : MoveProcessor
+    {
+    }
+}

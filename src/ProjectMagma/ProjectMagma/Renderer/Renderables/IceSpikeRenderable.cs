d91ab27505213664144c99e80515d751d01d8d69
kaeserd <kaeserd@fc16ba98-2866-4055-be8f-df951a5456ab>
2009-04-30 15:58:03 +0000

    git-svn-id: https://svn.vis.ethz.ch/svn/kob_gamelab09/trunk@761 fc16ba98-2866-4055-be8f-df951a5456ab

--- src/ProjectMagma/ProjectMagma/Renderer/Renderables/IceSpikeRenderable.cs
+++ src/ProjectMagma/ProjectMagma/Renderer/Renderables/IceSpikeRenderable.cs
@@ -156,7 +156,7 @@ namespace ProjectMagma.Renderer
         private Vector3 direction;
         private bool dead;
 
-        private float iceSpikeModelScale = 15;
+        private float iceSpikeModelScale = 135;
         private PointEmitter iceSpikeEmitter;
         private IceSpike iceSpikeSystem;
         Model iceSpikeModel;

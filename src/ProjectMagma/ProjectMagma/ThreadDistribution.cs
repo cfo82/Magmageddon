35360ab2dada843fedeea0ac2787916166616e2e
coberhol <coberhol@fc16ba98-2866-4055-be8f-df951a5456ab>
2009-05-02 18:26:08 +0000

    git-svn-id: https://svn.vis.ethz.ch/svn/kob_gamelab09/trunk@808 fc16ba98-2866-4055-be8f-df951a5456ab

---
+++ src/ProjectMagma/ProjectMagma/ThreadDistribution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMagma
+{
+    public class ThreadDistribution
+    {
+        public static readonly int RenderThread = 4;
+        public static readonly int SimulationThread = 1;
+        public static readonly int[] CollisionThreads = { 1, 3 };
+    }
+}

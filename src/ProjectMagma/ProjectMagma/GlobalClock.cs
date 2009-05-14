e9a987ef9a0b8336ac462f1b6783aa845a21a42a
coberhol <coberhol@fc16ba98-2866-4055-be8f-df951a5456ab>
2009-05-14 20:20:22 +0000

    git-svn-id: https://svn.vis.ethz.ch/svn/kob_gamelab09/trunk@1087 fc16ba98-2866-4055-be8f-df951a5456ab

---
+++ src/ProjectMagma/ProjectMagma/GlobalClock.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMagma
+{
+    public class GlobalClock
+    {
+        public GlobalClock()
+        {
+        	this.running = false;
+        	this.paused = false;
+        	this.continuousStartTick = this.pausableStartTick = 0;
+        	this.pauseTick = 0;
+        }
+
+		public void Start()
+		{
+			lock (this)
+			{
+				Debug.Assert(!this.paused);
+				Debug.Assert(!this.running);
+				this.running = true;
+                this.paused = true;
+				this.continuousStartTick = this.pausableStartTick = this.pauseTick = DateTime.Now.Ticks;
+			}
+		}
+		
+		public void Pause()
+		{
+			lock (this)
+			{
+				Debug.Assert(!this.paused);
+				this.paused = true;
+				this.pauseTick = DateTime.Now.Ticks;
+			}
+		}
+		
+		public void Resume()
+		{
+			lock (this)
+			{
+				Debug.Assert(this.paused);
+				long resumeTick = DateTime.Now.Ticks;
+				long tickDiff = resumeTick - this.pauseTick;
+				this.pausableStartTick += tickDiff;
+				this.paused = false;
+			}
+		}
+		
+		public void Stop()
+		{
+			lock (this)
+			{
+				this.running = false;
+			}
+		}
+		
+		private double CalculateMilliseconds(
+			long nowTick,
+			long startTick
+		)
+		{
+			long nowTime = nowTick - startTick;
+			return nowTime / 10000d;
+		}
+		
+		public double ContinuousMilliseconds
+		{
+			get
+			{
+				lock (this)
+				{
+					Debug.Assert(this.running);
+					return CalculateMilliseconds(DateTime.Now.Ticks, this.continuousStartTick);
+				}
+	        }
+		}
+		
+		public double ContinuousSeconds
+		{
+			get
+			{
+				return ContinuousMilliseconds / 1000d;
+	        }
+		}
+		
+		public double PausableMilliseconds
+		{
+			get
+			{
+				lock (this)
+				{
+					Debug.Assert(this.running);
+					return CalculateMilliseconds(
+						this.paused ? this.pauseTick : DateTime.Now.Ticks,
+						this.pausableStartTick
+					);
+				}
+	        }
+		}
+		
+		public double PausableSeconds
+		{
+			get
+			{
+				return PausableMilliseconds / 1000d;
+	        }
+		}
+		
+		private bool running;
+		private bool paused;
+        private long continuousStartTick;
+        private long pausableStartTick;
+        private long pauseTick;
+    }
+}

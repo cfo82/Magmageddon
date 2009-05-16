ce2aab1705d629d0443ceabd1a04f178a1695cef
jabernet <jabernet@fc16ba98-2866-4055-be8f-df951a5456ab>
2009-05-16 23:54:49 +0000

    non-moving island
    git-svn-id: https://svn.vis.ethz.ch/svn/kob_gamelab09/trunk@1133 fc16ba98-2866-4055-be8f-df951a5456ab

---
+++ src/ProjectMagma/ProjectMagma/Simulation/Properties/Island/IslandNoMovementControllerProperty.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using ProjectMagma.Simulation.Collision;
+
+
+namespace ProjectMagma.Simulation
+{
+    public class IslandNoMovementControllerProperty : IslandControllerPropertyBase
+    {
+        public IslandNoMovementControllerProperty()
+        {
+        }
+
+        public override void OnAttached(Entity entity)
+        {
+            base.OnAttached(entity);
+
+            originalPositon = entity.GetVector3("position");
+
+            if(entity.HasAttribute("fixed"))
+                fixedPosition = entity.GetBool("fixed");
+        }
+
+        public override void OnDetached(Entity entity)
+        {
+            base.OnDetached(entity);
+        }
+
+        public override void CalculateNewPosition(Entity island, ref Vector3 position, float dt)
+        {
+            // do nothing
+        }
+
+        protected override Vector3 GetNearestPointOnPath(ref Vector3 position)
+        {
+            if (fixedPosition)
+                return originalPosition;
+            else
+                return position;
+        }
+
+        protected override void CollisionHandler(SimulationTime simTime, Entity island, Entity other, Contact co, ref Vector3 normal)
+        {
+            // do nothing
+        }
+
+
+        private bool fixedPosition = true;
+        private Vector3 originalPositon;
+    }
+}

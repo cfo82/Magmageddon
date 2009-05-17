5e87e55d97910ca06161ac87ed8966404fb01663
jabernet <jabernet@fc16ba98-2866-4055-be8f-df951a5456ab>
2009-05-17 23:22:58 +0000

    git-svn-id: https://svn.vis.ethz.ch/svn/kob_gamelab09/trunk@1176 fc16ba98-2866-4055-be8f-df951a5456ab

---
+++ src/ProjectMagma/ProjectMagma/Simulation/Properties/Environment/PowerUpControllerLives.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Xna.Framework;
+
+using ProjectMagma.Simulation.Collision;
+using ProjectMagma.Simulation.Attributes;
+using Microsoft.Xna.Framework.Audio;
+
+namespace ProjectMagma.Simulation
+{
+    public class PowerUpControllerLives : PowerUpControllerBase
+    {
+
+        protected override void GivePower(Entity player)
+        {
+            player.SetInt("lives", player.GetInt("lives") + powerup.GetInt("value"));
+        }
+
+        protected override string NotificationString
+        {
+            get { return "EXTRA LIVES"; }
+        }
+
+    }
+}

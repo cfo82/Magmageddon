36d4b73ee7a79cb952d58c1268c5d6ca5afce878
coberhol <coberhol@fc16ba98-2866-4055-be8f-df951a5456ab>
2009-05-18 22:00:04 +0000

    release notes written
    git-svn-id: https://svn.vis.ethz.ch/svn/kob_gamelab09/trunk@1248 fc16ba98-2866-4055-be8f-df951a5456ab

--- src/ProjectMagma/ProjectMagma/Menu/ReleaseNotesMenu.cs
+++ src/ProjectMagma/ProjectMagma/Menu/ReleaseNotesMenu.cs
@@ -5,14 +5,20 @@ namespace ProjectMagma
 {
     class ReleaseNotesMenu : MenuScreen
     {
-        string funnyReleaseNotes =
+        private static readonly string funnyReleaseNotes =
             "Improvements from the May 19 release compared to the May 7 release:\n" +
-            "  - point 1\n" +
-            "  - point 2\n" +
-            "  - point 3\n" +
-            "  - point 4\n" +
-            "  - point 5\n" +
-            "  - point 6\n";
+            "  - New Control Possibilites: Use repulsion (LT) to move the island you\n" + 
+            "        are occupying, jump to other islands as soon as they are in range\n" + 
+            "        (indicated by an arrow)\n" +
+            "  - Check out out the new levels: Gameplay feels different on those maps.\n" + 
+            "  - Watch out for power ups. They help a lot if you are short on health or energy!\n" +
+            "  - If you are pushed off an island, save yourself using the jetpack on button A!\n" + 
+            "        If the opponent tries to safe himself, take him down using an ice spike\n" + 
+            "        (disables the jetpack)\n" + 
+            "  - Please report any bluescreens you might encounter! It is so easy, take a picture\n" + 
+            "        and mail it to us: dpk@student.ethz.ch!\n" + 
+            "  - Enjoy the game and admire the graphics ;-)\n";
+        private string[] funnyReleaseNotesLines = funnyReleaseNotes.Split('\n');
 
         public ReleaseNotesMenu(Menu menu)
             : base(menu, new Vector2(30, 650))
@@ -23,8 +29,17 @@ namespace ProjectMagma
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
             SpriteFont f = Game.Instance.ContentManager.Load<SpriteFont>("Fonts/menu_releasenotes");
-            Menu.DrawCenteredShadowString(spriteBatch, f, "RELEASE NOTES", new Vector2(620, 220), menu.StaticStringColor, 1.0f);
-            Menu.DrawShadowString(spriteBatch, f, funnyReleaseNotes, new Vector2(220, 240), menu.StaticStringColor, 0.66f);
+            Menu.DrawCenteredShadowString(spriteBatch, f, "RELEASE NOTES", new Vector2(620, 120), menu.StaticStringColor, 1.0f);
+
+            string[] lines = funnyReleaseNotesLines;
+
+            float offset = 0;
+            for (int i = 0; i < lines.Length; ++i)
+            {
+                if (lines[i].StartsWith("  -")) { offset += 10; }
+                Menu.DrawShadowString(spriteBatch, f, lines[i], new Vector2(220, 140 + offset), menu.StaticStringColor, 0.66f);
+                offset += 30;
+            }
         }
     }
 }

c00b9de60636a29e364b7eed3e18ecfed7c558a8
kaeserd <kaeserd@fc16ba98-2866-4055-be8f-df951a5456ab>
2009-05-24 18:56:13 +0000

    git-svn-id: https://svn.vis.ethz.ch/svn/kob_gamelab09/trunk@1491 fc16ba98-2866-4055-be8f-df951a5456ab

--- src/ProjectMagma/ProjectMagma/Game.cs
+++ src/ProjectMagma/ProjectMagma/Game.cs
@@ -158,7 +158,8 @@ namespace ProjectMagma
              
 #if DEBUG
             // initialize simulation
-            LoadLevel("Level/Instances/StaircaseOfDoom/Simulation", "Level/Instances/StaircaseOfDoom/Renderer");
+            //LoadLevel("Level/Instances/StaircaseOfDoom/Simulation", "Level/Instances/StaircaseOfDoom/Renderer");
+            LoadLevel("Level/Instances/4Noobs/Simulation", "Level/Instances/4Noobs/Renderer");
 
             // set default player
             Entity player1 = new Entity("player1");

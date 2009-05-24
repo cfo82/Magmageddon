a61edc1e22e6fcbf72dde2d507d7c63ee7f267bf
jabernet <jabernet@fc16ba98-2866-4055-be8f-df951a5456ab>
2009-05-24 17:51:04 +0000

    git-svn-id: https://svn.vis.ethz.ch/svn/kob_gamelab09/trunk@1486 fc16ba98-2866-4055-be8f-df951a5456ab

---
+++ src/ProjectMagma/ProjectMagma/Simulation/Properties/Player/RespawnLightControllerProperty.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using ProjectMagma.Framework.Attributes;
+using ProjectMagma.Shared.Math.Primitives;
+using ProjectMagma.Framework;
+
+namespace ProjectMagma.Simulation
+{
+    public class RespawnLightControllerProperty : Property
+    {
+
+        public RespawnLightControllerProperty()
+        {
+        }
+
+        public void OnAttached(AbstractEntity light)
+        {
+            this.light = light as Entity;
+            this.island = Game.Instance.Simulation.EntityManager[light.GetString("island")];
+
+            // register island change handler
+            island.GetVector3Attribute("position").ValueChanged += OnIslandPositionChanged;
+
+            positionOffset = light.GetVector3("position") - island.GetVector3("position");
+
+//            (light as Entity).Update += OnUpdate;
+        }
+
+        public void OnDetached(
+            AbstractEntity light
+        )
+        {
+//            (light as Entity).Update -= OnUpdate;
+
+            island.GetVector3Attribute("position").ValueChanged -= OnIslandPositionChanged;
+        }
+
+
+        private void OnUpdate(Entity light, SimulationTime simTime)
+        {
+        }
+
+
+        private void OnIslandPositionChanged(Vector3Attribute sender,
+            Vector3 oldValue, Vector3 newValue)
+        {
+            light.SetVector3("position", newValue + positionOffset);
+        }
+
+        private Vector3 positionOffset;
+        private Entity island;
+        private Entity light;
+    }
+}

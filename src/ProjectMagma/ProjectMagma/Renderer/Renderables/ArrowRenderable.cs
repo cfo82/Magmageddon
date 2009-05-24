25a7ca365c1ef1e62516c170335fd49f76f8b7b0
kaeserd <kaeserd@fc16ba98-2866-4055-be8f-df951a5456ab>
2009-05-24 15:19:51 +0000

    git-svn-id: https://svn.vis.ethz.ch/svn/kob_gamelab09/trunk@1478 fc16ba98-2866-4055-be8f-df951a5456ab

---
+++ src/ProjectMagma/ProjectMagma/Renderer/Renderables/ArrowRenderable.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ProjectMagma.Renderer
+{
+    public class ArrowRenderable : BasicRenderable
+    {
+        public ArrowRenderable(
+            double timestamp,
+            Vector3 scale, Quaternion rotation, Vector3 position, Model model)
+            //Vector3 color1, Vector3 color2)
+        :   base(timestamp, scale, rotation, position, model)
+        {
+            //Color1 = color1;
+            //Color2 = color2;
+        }
+
+        public override void LoadResources(Renderer renderer)
+        {
+            base.LoadResources(renderer);
+        }
+
+        protected override void ApplyTechnique(Effect effect)
+        {
+            effect.CurrentTechnique = effect.Techniques["UnicoloredAlpha"];
+        }
+
+        protected override void SetDefaultMaterialParameters()
+        {
+            Alpha = 0.75f;
+            //DiffuseColor = Color1 * 1.5f;
+            //SpecularColor = Color2 * 2.0f;
+            //EmissiveColor = Vector3.One * 0.3f;
+            SpecularPower = 1f;
+        }
+
+        protected override void ApplyCustomEffectParameters(Effect effect, Renderer renderer)
+        {
+            base.ApplyCustomEffectParameters(effect, renderer);
+
+           // effect.Parameters["LavaLightStrength"].SetValue(1.0f);
+        }
+
+        public override RenderMode RenderMode
+        {
+            get
+            {
+                return RenderMode.RenderToSceneAlpha;
+            }
+        }
+        //public Vector3 Color1 { get; set; }
+        //public Vector3 Color2 { get; set; }
+    }
+}

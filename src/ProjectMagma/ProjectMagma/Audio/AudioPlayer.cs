18a4ce5e1d18cf270298f9c4c4176bc9771e8b6c
coberhol <coberhol@fc16ba98-2866-4055-be8f-df951a5456ab>
2009-05-25 20:53:03 +0000

    git-svn-id: https://svn.vis.ethz.ch/svn/kob_gamelab09/trunk@1539 fc16ba98-2866-4055-be8f-df951a5456ab

--- src/ProjectMagma/ProjectMagma/Audio/AudioPlayer.cs
+++ src/ProjectMagma/ProjectMagma/Audio/AudioPlayer.cs
@@ -2,13 +2,86 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Audio;
 
 namespace ProjectMagma.Audio
 {
     public class AudioPlayer
     {
+        AudioPlayer()
+        {
+            handlesDistributed = new Dictionary<AudioHandle, SoundEffectInstance>();
+        }
 
+        public SoundEffectInstance Play(string sound)
+        {
+            EnsureEffectInstanceAvailable();
+            string chosenSound = PickOne(sound);
+            SoundEffect soundEffect = Game.Instance.ContentManager.Load<SoundEffect>(chosenSound);
+            SoundEffectInstance instance = soundEffect.Play();
+            effectInstances.Add(instance);
+            return instance;
+        }
 
+        public SoundEffectInstance Play(string sound, float volume)
+        {
+            EnsureEffectInstanceAvailable();
+            string chosenSound = PickOne(sound);
+            SoundEffect soundEffect = Game.Instance.ContentManager.Load<SoundEffect>(chosenSound);
+            SoundEffectInstance instance = soundEffect.Play(volume);
+            effectInstances.Add(instance);
+            return instance;
+        }
 
+        public SoundEffectInstance Play(string sound, float volume, float pitch, float pan, bool loop)
+        {
+            EnsureEffectInstanceAvailable();
+            string chosenSound = PickOne(sound);
+            SoundEffect soundEffect = Game.Instance.ContentManager.Load<SoundEffect>(chosenSound);
+            SoundEffectInstance instance = soundEffect.Play(volume * Game.Instance.EffectsVolume, pitch, pan, loop);
+            effectInstances.Add(instance);
+            return instance;
+        }
+
+        private string PickOne(string soundList)
+        {
+            string[] sounds = soundList.Split(' ');
+            return sounds[random.Next(0, sounds.Length - 1)];
+        }
+
+        private void EnsureEffectInstanceAvailable()
+        {
+            if (effectInstances.Count >= collectionThreshold)
+            {
+                List<SoundEffectInstance> obsoleteInstances = new List<SoundEffectInstance>();
+                foreach (SoundEffectInstance instance in effectInstances)
+                {
+                    if (instance.IsDisposed || instance.State == SoundState.Stopped)
+                    {
+                        obsoleteInstances.Add(instance);
+                    }
+                }
+
+                foreach (SoundEffectInstance instance in obsoleteInstances)
+                {
+                    effectInstances.Remove(instance);
+                    if (!instance.IsDisposed)
+                    {
+                        instance.Dispose();
+                    }
+                }
+            }
+
+            if (effectInstances.Count >= maxSoundEffectInstances)
+            {
+                throw new Exception("no more sound effect instances are available!");
+            }
+        }
+
+        private static readonly int collectionThreshold = 50;
+        private static readonly int maxSoundEffectInstances = 200;
+        private Dictionary<AudioHandle, SoundEffectInstance> handlesDistributed;
+        private HashSet<SoundEffectInstance> effectInstances;
+        private static Random random = new Random();
     }
 }
